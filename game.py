85f96a957ce4d4cfe1df0e3cee258db38d79859a
nabilkhalil <duomaster01@gmail.com>
2018-02-16 01:21:30 -0800

    added collision and  a 'Game Over'

--- game.py
+++ game.py
@@ -50,6 +50,27 @@ def StartMenu():
         screen.blit(TextSurf, TextRect)
         pygame.display.update()
         fpsClock.tick(15)
+
+def gameOver():
+    while gameOver:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        key = pygame.key.get_pressed()
+        if key[pygame.K_RETURN]:
+                print("Game Has Now Begun")
+                gameLoop()
+        screen.fill(black)
+        largeText = pygame.font.Font('./assets/Anton-Regular.ttf',34)
+        TextSurf, TextRect = text_objects("Game Over", largeText)
+        TextSurf2, TextRect2 = text_objects("Press Enter To Restart", largeText)
+        TextRect.center = ((width/2),(height/2))
+        TextRect2.center = ((width/2),(height/2 - 100))
+        screen.blit(TextSurf, TextRect)
+        screen.blit(TextSurf2, TextRect2)
+        pygame.display.update()
+        fpsClock.tick(15)
             
 
 # Player (Player playing as "Blue")
@@ -59,7 +80,6 @@ class Player():
         self.rect = self.image.get_rect()
         self.rect.x = 540
         self.rect.y = 500
-        self.velocity = 0
     def update(self):
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
@@ -76,14 +96,19 @@ class Anvil():
     def __init__(self, x, y):
         self.image = pygame.transform.scale(pygame.image.load("./assets/anvil.png"), (120,120))
         self.rect = self.image.get_rect()
-        self.x = x
-        self.y = 0
-        self.velocity = 0
+        self.rect.x = x
+        self.rect.y = 0
     def update(self):
-        self.rect.y += 1
+        self.rect.y += 20
+        if anvil.rect.colliderect(player.rect):
+            print("you touched me")
+            gameOver()
     def render(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 anvil = Anvil(0,0)
+
+
+
 
 # Game loop
 
@@ -106,32 +131,25 @@ def gameLoop():
                 sys.exit()
           
         # Adds Anvil Every 3 Seconds
+        anvilCounter = 0
         if dt >= 2:
-            print(dt)
             newX = random.randint(1,1280)
             print ("Anvil Spawnd At X Cord:",newX)
-            anvil.x = newX
-            print ("Three seconds reached, resetting timer")
+            anvil.rect.x = newX
+            print ("2 seconds reached, resetting timer")
             anvils.append(anvil)
             t0 = t1
             print(anvils)
             
-            
-        anvil.update()
-        anvil.render(screen)
+
         for anvil in anvils:
-            if player.rect.colliderect(anvil.rect):
-                print("you touched me")
-            if anvil.y >= 510:
-                anvil.y = 0
+            print("Anvil Y Cord:",anvil.rect.y)
+            if anvil.rect.y >= 500:
+                anvil.rect.y = 0
                 anvils.remove(anvils[0])
-
         
-            
-            
-        
-        
-
+            anvil.update()
+            anvil.render(screen)
 
         # Update.
         player.update()
